00202fcca82e0313a800cf6d4e8a7d4c3226ad75
syed shahabaaz ahmed <shahabaazahmedsyed@gmail.com>
2020-05-21 16:21:39 +0530

    feature to extract and display question and answer from database with desired priority

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ priority integer,
 instance integer)''')
 
 def insert(q,a,d):
-    c.execute('''INSERT INTO stack (question,answer,deck,priority,instance) VALUES (?,?,?,?,?)''',[q,a,d,0,0])
+    c.execute('''INSERT INTO stack (question,answer,deck,priority,instance) VALUES (?,?,?,?,?)''',[q,a,d,3,0])
     conn.commit()
 
 def fetch():
@@ -22,3 +22,19 @@ def fetch():
     rows = list(dict.fromkeys(rows))
     return rows
 
+
+def fetch_prio():
+    cur = conn.cursor()
+    cur.execute("SELECT question, answer, id FROM stack WHERE priority = 3")
+
+    rows = cur.fetchall()
+
+    q = []
+    a = []
+    i = []
+    for i in range(len(rows)):
+        q.append(rows[i][0])
+        a.append(rows[i][1])
+        # i.append(rows[i][2])
+
+    return q, a
--- flash_gui.py
+++ flash_gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import font
 import database
 
+q, a = database.fetch_prio()
 
 root = tk.Tk()
 canvas = tk.Canvas(root, height=720, width=1280, bg='white')
@@ -78,6 +79,7 @@ def inputdata(qbox, abox, entry):
 
 
 def nexttab():
+    
     frame = tk.Frame(root, bg='purple', bd=4)
     frame.place(relheight=0.75, relwidth=0.9, relx=0.5, rely=0.1, anchor='n')
     label = tk.Label(frame, font=50, bg='white', fg='black')
@@ -94,13 +96,18 @@ def nexttab():
     button = tk.Button(frame, text="Hard", font=40,
                        bg='white', fg='black', command=lambda: open_deck())
     button.place(relx=0.78, rely=0.75, relwidth=0.20, relheight=0.2)
-    ans = "Answer"
+    
     label.config(font=("Courier", 44))
-    label['text'] = ans
-   
+    try:
+        answer = a.pop()
+        label['text'] = answer
+    except:
+        label['text'] = "answer not found"
+
 
 
 def open_deck():
+
     canvas = tk.Canvas(root, height=720, width=1280, bg='white')
     canvas.pack()
     backimg = tk.PhotoImage(file='nature.png')
@@ -111,7 +118,12 @@ def open_deck():
     label = tk.Label(frame, font=50, bg='white', fg='black')
     label.place(relwidth=1, relheight=0.7)
     label.config(font=("Courier", 44))
-    label['text'] = "Question"
+    
+    try:
+        question = q.pop()
+        label['text'] = question
+    except:
+        label['text'] = "question not found"
     button = tk.Button(frame, text="Show Answer", font=40,
                        bg='white', fg='black', command=lambda: nexttab())
     button.place(relx=0.35, rely=0.75, relwidth=0.3, relheight=0.2)
